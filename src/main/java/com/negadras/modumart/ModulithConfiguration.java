1e997fa8d340edcdb112b22b9bea7e3ccda4406f
negadras <negamdras@gmail.com>
2025-08-12 00:00:21 +0200

    feat: configure database and initial Spring Modulith structure

---
+++ src/main/java/com/negadras/modumart/ModulithConfiguration.java
@@ -0,0 +1,9 @@
+package com.negadras.modumart;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.modulith.events.config.EnablePersistentDomainEvents;
+
+@Configuration
+@EnablePersistentDomainEvents
+public class ModulithConfiguration {
+}
