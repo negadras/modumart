cdbf045817d1770aeca2c937981fb3038f1ba7ec
negadras <negamdras@gmail.com>
2025-08-11 23:45:04 +0200

    feat: project to demo Spring Modulith

---
+++ src/main/java/com/negadras/modumart/ModumartApplication.java
@@ -0,0 +1,13 @@
+package com.negadras.modumart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModumartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModumartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/negadras/modumart/ModumartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negadras.modumart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModumartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
